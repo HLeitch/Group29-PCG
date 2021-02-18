9bdd66bc2d1d8c000daea9ccb4b796e2724e3178
Josh Kitchen <45073406+josh42k@users.noreply.github.com>
2021-02-18 14:37:56 +0000

    Player Created
    Normalised movement
    Simple animations
    Simple particles
    Weapon weight changes speed

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float drag;
+    public float weapon_weight;
+    Rigidbody2D rb;
+    Vector2 movement;
+    SpriteRenderer spr;
+    ParticleSystem parts;
+    Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spr = GetComponentInChildren<SpriteRenderer>();
+        parts = GetComponent<ParticleSystem>();
+        anim = GetComponentInChildren<Animator>();
+        anim.StopPlayback();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x < 0) spr.flipX = true;
+        else if (movement.x > 0) spr.flipX = false;
+
+        weapon_weight = Mathf.Clamp(weapon_weight, 0, 1);
+        rb.velocity += movement.normalized * (speed * (1-weapon_weight)) * Time.deltaTime;
+
+        if (rb.velocity.magnitude < 1)
+        {
+            rb.velocity = Vector2.zero;
+            if (parts.isPlaying) parts.Stop();
+        }
+        else
+        {
+            if (!parts.isPlaying) parts.Play();
+        }
+        anim.SetFloat("Velocity", rb.velocity.magnitude);
+
+        rb.velocity *= drag;
+    }
+}
