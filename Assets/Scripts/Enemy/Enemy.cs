67773d605cd65f4639cf852a609966f696d149a3
HLeitch <55984810+HLeitch@users.noreply.github.com>
2021-02-19 17:00:59 +0000

    UI Package Merged

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject weapon;
+    public Transform weaponHoldPoint;
+
+    public Rigidbody2D rigidbody;
+    public float moveSpeed;
+    public float health;
+
+    public float maxHealth = 50f;
+
+    public EnemyHealthBar healthBar;
+
+    public Transform healthBarPostitionSet;
+
+    UIManager uimanager;
+    WeaponManager wp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        moveSpeed = 2;
+        health = maxHealth;
+
+        uimanager = FindObjectOfType<UIManager>();
+
+        wp = FindObjectOfType<WeaponManager>();
+        weapon = wp.GenerateWeapon();
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        healthBar.targetPosition = healthBarPostitionSet;
+
+        if (Input.GetKeyUp(("e")))
+        {
+
+
+            ChangeHealth(-10f);
+        }
+
+        weapon.transform.position = weaponHoldPoint.transform.position;
+    }
+
+
+
+
+    void ChangeHealth(float Value)
+    {
+
+        health += Value;
+        healthBar.ChangeHealth(health/maxHealth);
+
+        if (health <=0)
+        {
+
+            //Die();
+
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Enemy/EnemyHealthBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : UIHealthBar
+{
+    public Transform targetPosition;
+
+    private Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMaskSize = healthBarMask.rectTransform.rect.width;
+        healthPercentage = 1;
+        HealthBarValue = 1;
+
+        canvas = FindObjectOfType<UIManager>().gameObject.GetComponent<Canvas>();
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Camera.main.WorldToScreenPoint(targetPosition.position);
+
+
+             if (HealthBarValue != healthPercentage)
+        {
+
+
+            float newHealthBarValue = Mathf.SmoothDamp(HealthBarValue, healthPercentage, ref shrinkVelocity, 0.5f);
+
+            HealthBarValue = newHealthBarValue;
+            SetHealthBarValue(HealthBarValue);
+
+        }
+
+    }
+}
