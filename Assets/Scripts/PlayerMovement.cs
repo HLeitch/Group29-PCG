75e1d8ebcd9c55dd0e52622c239d9dc291610dd4
HLeitch <55984810+HLeitch@users.noreply.github.com>
2021-02-20 13:01:13 +0000

    Player merged

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float drag;
+    //[HideInInspector]
+    public float weapon_weight;
+    Rigidbody2D rb;
+    Vector2 movement;
+    //PlayerCombat combat;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //combat = GetComponent<PlayerCombat>();
+    }
+
+    void Update()
+    {
+        // Assign axis data to vector
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Restrict the weapon weight to be 0-1
+        weapon_weight = Mathf.Clamp(weapon_weight, 0, 1); // This will be removed once the player can have a weapon
+
+        // Increase the velocity vector
+        rb.velocity += movement.normalized * (speed * (1-weapon_weight)) * Time.deltaTime;
+
+        // Apply the drag
+        rb.velocity *= drag;
+    }
+}
---
+++ Assets/Scripts/PlayerVisuals.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisuals : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer spr;
+    ParticleSystem parts;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spr = GetComponentInChildren<SpriteRenderer>();
+        parts = GetComponent<ParticleSystem>();
+        anim = GetComponentInChildren<Animator>();
+        anim.StopPlayback();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.x < 0) spr.flipX = true;
+        else if (rb.velocity.x > 0) spr.flipX = false;
+
+        if (rb.velocity.magnitude < 1)
+        {
+            rb.velocity = Vector2.zero;
+            if (parts.isPlaying) parts.Stop();
+        }
+        else
+        {
+            if (!parts.isPlaying) parts.Play();
+        }
+        anim.SetFloat("Velocity", rb.velocity.magnitude);
+    }
+}
