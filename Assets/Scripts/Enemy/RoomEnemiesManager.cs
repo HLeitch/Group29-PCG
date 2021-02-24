86526daf13908062b7ebbb4c3651eec27830a4b8
HLeitch <55984810+HLeitch@users.noreply.github.com>
2021-02-24 15:07:51 +0000

    EnemyManager created

---
+++ Assets/Scripts/Enemy/RoomEnemiesManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomEnemiesManager : MonoBehaviour
+{
+
+    public Enemy[] enemiesInRoom;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemiesInRoom = GetComponentsInChildren<Enemy>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
