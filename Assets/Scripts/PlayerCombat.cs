7cf43ccdd745fdaad47b9fae77b7c76c38d41d52
Jordan <jordylee264@gmail.com>
2021-02-25 15:41:09 +0000

    Working with rest of stuff

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -7,22 +7,35 @@ public class PlayerCombat : MonoBehaviour
     public int max_hp;
     int hp;
     GameObject weapon;
+    public WeaponManager wm;
+    private float timeBetweenAttack;
+    public bool swinging = false;
+    public float startTimeBetweenAttack;
+    private Animator weaponAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         hp = max_hp;
+        startTimeBetweenAttack = timeBetweenAttack;
+        weaponAnimator = transform.Find("Pivot").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(SwingWeapon());
         // Attack code is handled by the weapon (i think)
+        if (Input.GetKeyDown("m"))
+        {
+            GetNewWeapon();
+        }
     }
 
     public void GetNewWeapon()
     {
         // TODO: Use 'WeaponManager' to generate a new weapon and add it as a child to the player, and reference it with the 'weapon' variable
+        wm.ChooseWeaponType();
         Physics2D.IgnoreCollision(transform.Find("HitCollider").GetComponent<Collider2D>(), weapon.GetComponent<Collider2D>()); // Stop the player colliding with their own weapon
     }
 
@@ -52,5 +65,24 @@ public class PlayerCombat : MonoBehaviour
         {
 
         }
+    }
+
+    IEnumerator SwingWeapon()
+    {
+        if (timeBetweenAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                weaponAnimator.SetBool("Swinging", true);
+                swinging = true;
+                yield return new WaitForSeconds(0.3f);
+                weaponAnimator.SetBool("Swinging", false);
+                yield return new WaitForSeconds(0.15f);
+                swinging = false;
+                timeBetweenAttack = startTimeBetweenAttack;
+            }
+        }
+        else
+            timeBetweenAttack -= Time.deltaTime;
     }
 }
