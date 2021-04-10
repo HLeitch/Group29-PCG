355f6e7e139ca17d3a4ed0cc7c74e55abe714e49
Josh Kitchen <45073406+josh42k@users.noreply.github.com>
2021-04-10 17:42:26 +0100

    Start, options and credits screens
    Start screen has 3 buttons:
    Play
    Options
    Credits
    
    Credits screen is just text, needs other asset creators added and maybe twitters/linkedins/githubs/websites to make it easier to find people
    
    Options screen has 2 sliders, Sound and Music. they each control a PlayerPref (float) of the same name and *should* update the AudioSource objects correctly in the current scene, but this functionality still needs adding to all scenes
    
    Any questions on how stuff works/feedback, just message me on teams :D

---
+++ Assets/Scripts/UI/ChangeSceneButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Used by the ChangeSceneButton prefab
+/// </summary>
+public class ChangeSceneButton : MonoBehaviour
+{
+    /// <summary>
+    /// Loads a scene, saving data before hand if neccessary.
+    /// </summary>
+    /// <param name="sceneName">
+    /// The name of the scene to load - must be added in the build settings!
+    /// </param>
+    public void ChangeScene(string sceneName)
+    {
+        // Save neccessary data (score, highscore, etc...) here
+        Fade fade = GameObject.FindObjectOfType<Fade>(); // Get current scene's fade object,
+
+        StartCoroutine(fade.toBlack((finished) => {          // Start a coroutine to fade the screen to black.
+            if (finished) SceneManager.LoadScene(sceneName); // When it finishes, load the new scene
+        }));
+    }
+}
---
+++ Assets/Scripts/UI/Fade.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [Tooltip("Any faster than 8 and it stops working...")]
+    [SerializeField]
+    private float speed;
+
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine(toClear());
+    }
+
+    public IEnumerator toClear()
+    {
+        for (float i = image.color.a; i >= 0; i -= speed * Time.deltaTime) // Repeat many times for a smooth fade,
+        {
+            Color new_color = image.color; // Get current colour,
+            new_color.a = i; // Reduce its opacity,
+            image.color = new_color; // Update the colour with our new one,
+            if (image.color.a <= 0.05f) GetComponent<Image>().enabled = false; // If it is practically invisible, deactivate it.
+
+            yield return null;
+        }
+    }
+
+    public IEnumerator toBlack(System.Action<bool> callback) // Callback allows for returning of variable (in this case, whether or not the coroutine has finished)
+    {
+        GetComponent<Image>().enabled = true;
+        for (float i = image.color.a; i <= 1; i += speed * Time.deltaTime) // Repeat many times for a smooth fade,
+        {
+            Color new_color = image.color; // Get current colour,
+            new_color.a = i; // Reduce its opacity,
+            image.color = new_color; // Update the colour with our new one, 
+
+            yield return null;
+        }
+        callback(true); // Return true (coroutine has finished)
+    }
+}
