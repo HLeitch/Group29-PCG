931a457028b43c6e09aba2cd59e9bbd84a08220b
HLeitch <55984810+HLeitch@users.noreply.github.com>
2021-04-19 14:14:45 +0100

    Start and options screens added

---
+++ Assets/Scripts/UI/SoundSlider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSlider : MonoBehaviour
+{
+    private Slider slider;
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        text = transform.Find("Value").GetComponent<Text>();
+        transform.Find("Label").GetComponent<Text>().text = gameObject.name;
+        slider.value = PlayerPrefs.GetFloat(gameObject.name);
+        setValueText(slider.value);
+        
+    }
+
+    public void UpdateSlider()
+    {
+        setValueText(slider.value);
+        PlayerPrefs.SetFloat(gameObject.name, slider.value);
+        foreach (AudioSource audioSource in GameObject.FindObjectsOfType<AudioSource>())
+        {
+            if (audioSource.gameObject.name == "MusicManager")
+            {
+                audioSource.volume = PlayerPrefs.GetFloat("Music");
+            }
+            else
+            {
+                audioSource.volume = PlayerPrefs.GetFloat("Sound");
+            }
+        }
+    }
+
+    private void setValueText(float value)
+    {
+        text.text = Mathf.RoundToInt(value * 100).ToString() + "%";
+    }
+}
